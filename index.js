5690e7aec6655fc5165043724a0ba97938809aa0
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-05 21:57:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(str="roller-skate"){
+    return `This Saturday, I want to ${str}!`
+}
+
+let mondayWork = function(par = "go to the office") {
+    return `This Monday, I will ${par}.`
+}
+
+function wrapAdjective(flair = '*') {
+     let innerfunction = function(arg = 'special') {
+        return `You are ${flair}${arg}${flair}!`
+    }
+    return innerfunction;
+}
+
+let Calculator = {
+  add: function(a, b) {
+      return a+b
+  }, 
+  subtract: function(a,b) {
+      return a-b
+  }, 
+  multiply: function(a,b) {
+      return a*b 
+  }, 
+  divide: function(a,b) {
+      return a/b
+  }
+}
+
+function actionApplyer(int, arr){
+    for (const fn of arr) {
+        int = fn(int)
+    }
+    return int
+}
