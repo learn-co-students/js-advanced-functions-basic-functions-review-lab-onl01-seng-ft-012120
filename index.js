2d7ea1a167d29da2c13cc0877e6903dac03095ee
github username <github email address>
2020-05-13 01:20:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function saturdayFun(name = 'roller-skate'){
+
+return `This Saturday, I want to ${name}!`
+
+}
+let mondayWork = function(act ='go to the office'){
+  return `This Monday, I will ${act}.`
+
+}
+
+function wrapAdjective(wrapper= "*"){
+
+  return (
+    function (adj= "special"){
+    
+      return  `You are ${wrapper}${adj} ${wrapper}!`
+      
+    }
+  )
+}
+// "You are %a dedicated programmer%!"
